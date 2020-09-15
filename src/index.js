5e3acf4c821a8dfca523b26da0f435c5adc72931
github username <github email address>
2020-09-15 17:35:55 -0500

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+ 
+const form = document.getElementById("create-task-form");
+
+
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  const comment = event.target['new-task-description'].value
+  
+  const liTag = document.createElement('li')
+  liTag.innerText = comment
+  const commentsContainer = document.querySelector("ul")
+  commentsContainer.append(liTag)
+  //appendLiTag(liTag);
+  form.reset()
+  //const appendLiTag = document.getElementsById("tasks").appendChild(liTag) 
+
+});
 });
