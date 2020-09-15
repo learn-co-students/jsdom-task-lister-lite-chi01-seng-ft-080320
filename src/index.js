114465fa73e2acedf255054142d30eaa11bed98f
Brian Butterly <butterlybrian@gmail.com>
2020-09-15 16:25:26 -0500

    main goals done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+main()
+
+  function createFormListener(){
+    const form = document.querySelector("form")
+    form.addEventListener("submit", function(event){
+    event.preventDefault()
+    const comment = event.target["new-task-description"].value
+    const pTag = document.createElement("li")
+    pTag.innerText = comment
+    const commentsContainer = document.querySelector("#tasks")
+    commentsContainer.append(pTag)
+    form.reset()
+  })
+  }
+
+  function createAlertBtnListener(){
+    const alertBtn = document.querySelector("#submitBtn")
+    alertBtn.addEventListener("click", function(){
+    alert("Whatever Bro")  
+    }
+    )
+  }
+
+  function main(){
+   createFormListener() 
+   createAlertBtnListener()
+  }
+
 });
