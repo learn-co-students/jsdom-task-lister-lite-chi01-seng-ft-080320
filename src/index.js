b04caf81faca69dee1cdf4119b604025e5198a34
Ramazan774 <ramazan.ramazanov@glion.ch>
2020-09-15 20:03:41 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const comment = event.target["new-task-description"].value
+
+    const liTag = document.createElement("li")
+    liTag.innerText = comment
+    const commentsContainer = document.querySelector("ul")
+    commentsContainer.append(liTag)
+    form.requestFullscreen()
+  })
 });
