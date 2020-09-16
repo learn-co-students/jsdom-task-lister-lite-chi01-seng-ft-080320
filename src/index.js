3bd6593e6d62e617c7df29c9e04ee03c9d9a5d25
github username <github email address>
2020-09-16 08:31:43 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+const taskForm = document.querySelector('form')
+taskForm.addEventListener('submit', function(event){
+  event.preventDefault()
+
+const comment = event.target["new-task-description"].value
+const name = event.target["name-field"].value
+
+const pTag2 = document.createElement('p')
+const pTag = document.createElement('p')
+pTag.innerText = comment
+pTag2.innerText = name
+
+const commentsContainer = document.querySelector('#tasks')
+const commentsContainer2 = document.querySelector('#tasks')
+commentsContainer.append(pTag2) 
+commentsContainer.append(pTag) 
+
+taskForm.reset()
+
+})
+
+
 });
