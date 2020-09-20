4cd51fc0f5d8f6b44f5bc64709038cabf443032e
thrlstl <matthewsteeleonline@gmail.com>
2020-09-20 15:25:33 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-//event listenters
-
 document.addEventListener("DOMContentLoaded", () => {
   const newTaskForm = document.getElementById("create-task-form");
   const newTaskDescription = document.getElementById("new-task-description");
